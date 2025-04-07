bb8e9e5f2e60242eb2c792d630b7bcf393ddd9a4
galsri03 <162865663+galsri03@users.noreply.github.com>
2025-04-07 14:28:15 +0000

    commit

--- Trio.java
+++ Trio.java
@@ -1,1 +1,32 @@
 //Write your answer here
+//I didn't make changes after the solution was given
+public class Trio implements MenuItem 
+{
+    Private Sandwich sandwich;
+    Private Salad salad;
+    Private Drink drink;
+
+public Trio(Sandwich sand, Salad sal, Drink drink)
+{
+    sandwich = sand;
+    salad = sal;
+    drink = dr;
+}
+
+public String getName()
+{
+    return sandwich.getName() + "/" + salad.getName() + "/" + drink.getName() + "/" + "Trio"; 
+}
+
+public Double getPrice()
+{
+    MenuItem cheapest = sandwich;
+
+    if (salad.getPrice < cheapest.getPrice())
+        cheapest = salad;
+
+    if (drink.getPrice < cheapest.getPrice())
+        cheapest = drink;
+}
+
+}
